13013b5eab9e6e2b73d18e7fa1c08f0c20f132ac
chunchuprati <prathibha.panga@gmail.com>
2017-09-16 18:15:56 -0700

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ var friendData 		= require('../data/friends.js');
 var path 			= require('path');
 
 
-var totalDifference = 0;
+
 
 module.exports = function(app){
 	app.get('/api/friends', function(req, res){
